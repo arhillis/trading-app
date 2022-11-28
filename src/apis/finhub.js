c2d972a7aae4044d28af9339eb8e3581990b170a
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-28 12:25:14 -0600

    Using Axios

--- src/apis/finhub.js
+++ src/apis/finhub.js
@@ -1,6 +1,17 @@
-export const fetchQuote = (fn) =>{
-        return fetch(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=cdue3rqad3ib3oqtedggcdue3rqad3ib3oqtedh0`)
-         .then(res => res.json())
-         .then(data => fn(data))
-    }
+import axios from 'axios';
+
+// export const fetchQuote = (fn) =>{
+//         return fetch(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=cdue3rqad3ib3oqtedggcdue3rqad3ib3oqtedh0`)
+//          .then(res => res.json())
+//          .then(data => fn(data))
+//     }
+
+const TOKEN = 'cdue3rqad3ib3oqtedggcdue3rqad3ib3oqtedh0';
+
+export default axios.create({
+        baseURL: 'https://finnhub.io/api/v1',
+        params: {
+                token: TOKEN
+        }
+})
 
