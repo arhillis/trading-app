b59c44736564a997ee917378578a44ab59e07321
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-30 20:20:15 -0600

    Basic searchbar, can get data

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,73 @@
+import {useState, useEffect} from 'react';
+import finnHub from '../apis/finhub';
+
 function SearchBar(){
-    return (<>Search Bar goes here...</>)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchList, setSearchList] = useState([])
+
+    const handleChange = (e) =>{
+        setSearchTerm(e.target.value)
+    }
+
+    const handleClick = (stock) =>{
+        console.log(stock);
+
+    }
+
+    useEffect(() =>{
+        if(searchTerm !== ""){
+            const fetchSearch = async () =>{
+                const results = await finnHub.get
+                                    ("/search", {
+                                        params: {
+                                            q: searchTerm
+                                        }
+                                    })
+                setSearchList(results.data.result)
+            }
+
+            fetchSearch();
+        }else{
+            setSearchList([])
+        }      
+    }, [searchTerm])
+
+    return (<div className="w-50 p-5 rounded mx-auto">
+                <div className="form-floating dropdown">
+                    <input  id="searchbar"
+                            className="form-control" 
+                            type="text"
+                            name='searchTerm'
+                            value={searchTerm}
+                            placeholder="Search..." 
+                            aria-label="searchbar"
+                            autoComplete="off"
+                            onChange={handleChange}
+                    ></input>
+                    <label htmlFor="searchbar">Search</label>
+                    {
+                        searchList.length > 0 ?
+                            (<ul className="dropdown-menu show"
+                                style={{
+                                    height: '20rem',
+                                    overflowY: 'scroll',
+                                    overflowX: 'hidden',
+                                    cursor: 'pointer'
+                                }}
+                            >
+                                {searchList.map((stock, index)=>(
+                                    <li key={stock.symbol}
+                                        onClick={() => handleClick(stock)}
+                                    >
+                                        {stock.description} ({stock.symbol})
+                                    </li>
+                                ))}
+                            </ul>) : 
+                            null
+                    }
+                    
+                </div>
+            </div>)
 }
 
 export default SearchBar;
