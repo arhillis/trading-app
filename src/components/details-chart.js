aad1e2c0ceac24c2b0b88db7b14fe9d0f5be6a5c
Andrielle Hillis <andriellerhillis@gmail.com>
2023-01-20 13:24:09 -0600

    Apex Charts added

--- src/components/details-chart.js
+++ src/components/details-chart.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from 'react';
+import Chart from "react-apexcharts";
 
-export default function DetailsChart() {
-  return (
-    <div>DetailsChart goes here..</div>
-  )
+export default function DetailsChart({stockData, symbol}) {
+    const {oneDay, oneWeek, oneYear} = stockData;
+
+    console.log(symbol);
+    console.log(stockData);
+
+    const options = {
+        title: {
+            text: symbol,
+            align: "center",
+            style: {
+                color: 'blue',
+                fontSize: '24px'
+            }
+        },
+        chart: {
+          id: "stock data",
+          animations: {
+            speed: 2500
+          }
+        },
+        xaxis: {
+            type: "datetime",
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+      };
+    const series = [
+        {
+          name: symbol,
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ];
+    return (
+        <div>
+            <Chart
+              options={options}
+              series={series}
+              type="line"
+              width="500"
+            />
+        </div>
+    )
 }
